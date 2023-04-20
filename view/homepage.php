586df91ecb9243d9f72ea0821ae70cc95acf82f1
Frédéric Favreau <frederic.favreau.dev@gmail.com>
2023-04-20 19:16:35 +0200

    strat hero

--- view/homepage.php
+++ view/homepage.php
@@ -4,6 +4,24 @@ $style = './CSS/style.css';
 $title = "Burger dev";
 ob_start();
 ?>
+<main>
+    <section id="hero-section">
+        <div id="container-hero-section" class="row-limit-size">
+            <div id="item-left-hero">
+                <span>Recettes Burger</span>
+                <h1>tasty & creamy</h1>
+                <p>Delicious burgers made from
+                    high-quality Australian beef, carefully processed to create a juicy and flavorful taste.</p>
+                    <a href="#" id="btn-show-recipe"><img src="./img/picto/bag-2.svg" alt="Sac">voir les recettes</a>
+            </div>
+            <figcaption id="item-right-hero">
+                <img src="./img/burger-mobile.png" alt="Burger">
+            </figcaption>
+
+        </div>
+
+    </section>
+</main>
 <div class="row">
     <h1><?= $title ?></h1>
     <div class="posts">
