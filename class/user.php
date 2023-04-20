5bac21f4134da781083525f6f8171b4f7e0b4ec2
frederic-favreau <frederic.favreau22@gmail.com>
2023-04-20 11:09:27 +0200

    user page class

---
+++ class/user.php
@@ -0,0 +1,65 @@
+<?php
+class user
+{
+    private $lastname;
+    private $firstname;
+    private $email;
+    private $password;
+    private $userId;
+
+    public function __construct(array $datas)
+    {
+        $this->hydrate($datas);
+    }
+
+    private function hydrate(array $datas)
+    {
+        foreach ($datas as $key => $value) {
+            $method = 'set' . ucfirst($key);
+
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    //GETTERS
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    //SETTERS
+    public function setLastname(string $lastname)
+    {
+        $this->lastname = $lastname;
+    }
+    public function setFirstname(string $firstname)
+    {
+        $this->firstname = $firstname;
+    }
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+    public function setUserId(int $userId)
+    {
+        $this->userId = $userId;
+    }
+}
